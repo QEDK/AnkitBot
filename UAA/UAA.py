75fcbd9e8cb7d35503ba52bbf5e6bc8316bde2a8
Ankit Maity <1994constant@gmail.com>
2014-11-04 10:12:54 +0530

    Localizing

--- UAA/UAA.py
+++ UAA/UAA.py
@@ -3,10 +3,10 @@ import sys, localconfig, platform, time
 #OS Runtime comments
 if platform.system() == "Windows":
         sys.path.append(localconfig.winpath)
-        print "You are running DeltaQuadBot UAA Module for Windows."
+        print "You are running the AnkitBot UAA Module for Windows. Sponsored by DQ. :)"
 else:
         sys.path.append(localconfig.linuxpath)
-        print "You are running DeltaQuadBot UAA Module for Linux."
+        print "You are running the AnkitBot UAA Module for Linux. Sponsored by DQ. :)"
 import wikipedia
 import globalfunc as globe
 override = False
