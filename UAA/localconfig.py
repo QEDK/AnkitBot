f209077d8e92e31ce6f54bcac1ecf7bcc15eb049
Ankit Maity <1994constant@gmail.com>
2014-11-04 09:22:56 +0530

    Localizing

--- UAA/localconfig.py
+++ UAA/localconfig.py
@@ -1,23 +1,23 @@
 ##Main Settings
-botname = "DeltaQuadBot"
+botname = "AnkitBot"
 
 ##Default System Path
 winpath = "C:\\pywikipedia\\"#Windows path for pywikipedia, need "\\" for folder switch
 linuxpath = "/data/project/deltaquad-bots/pywikipedia/pywikipedia"
 
 #Onwiki paths
-waitlist = "User:DeltaQuad/UAA/Wait"
-gopage = "User:DeltaQuad/UAA/Run"
-timepage = "User:DeltaQuad/UAA/Time"
-settings = "User:DeltaQuad/UAA/Settings"
-blacklist = 'User:DeltaQuad/UAA/Blacklist'
-whitelist = 'User:DeltaQuad/UAA/Whitelist'
-simlist =  'User:DeltaQuad/UAA/Similar'
+waitlist = "User:Ankit Maity/UAA/Wait" #The pages have not been made yet. NOTE TO SELF!
+gopage = "User:Ankit Maity/UAA/Run"
+timepage = "User:Ankit Maity/UAA/Time"
+settings = "User:Ankit Maity/UAA/Settings"
+blacklist = 'User:Ankit Maity/UAA/Blacklist'
+whitelist = 'User:Ankit Maity/UAA/Whitelist'
+simlist =  'User:Ankit Maity/UAA/Similar'
 postpage = "Wikipedia:Usernames for administrator attention/Bot"
 holdpage = "Wikipedia:Usernames for administrator attention/Holding pen"
 
 #Runtime edit summarties
 primarytaskname = "Task UAA listing"
-editsumtime = "[[User:DeltaQuadBot|DeltaQuadBot]] UAA Updating Run Time."
-editsumwait = "[[User:DeltaQuadBot|DeltaQuadBot]] Update UAA wait list."
-editsumclear = "[[User:DeltaQuadBot|DeltaQuadBot]] UAA Removing blocked users and moving waiting requests."
+editsumtime = "[[User:AnkitBot|AnkitBot]] UAA Updating Run Time."
+editsumwait = "[[User:AnkitBot|AnkitBot]] Update UAA wait list."
+editsumclear = "[[User:AnkitBot|AnkitBot]] UAA Removing blocked users and moving waiting requests."
